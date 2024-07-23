f41756cec80d6683dfbd342f04f790bec4f49267
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-23 20:47:56 -0300

    "navbar and footer in the layout, the Home, About and Contact routes are created"

---
+++ front/src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import ImgContainer from "@/components/ImgContainer/ImgContainer";
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <div className="flex min-h-screen flex-col ">
+      <div className="">
+        <ImgContainer src="/img-about.png" alt="Description for image" />
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,18 +1,11 @@
-import Navbar from "@/components/Navbar/Navbar";
 import ImgContainer from "@/components/ImgContainer/ImgContainer";
-import Footer from "@/components/Footer/Footer";
 
 export default function Landing() {
-	return (
-		<main>
-			<div className="relative">
-				<Navbar />
-				<ImgContainer
-					src="/img-landing.png"
-					alt="Description for image"
-				/>
-			</div>
-			<Footer />
-		</main>
-	);
+  return (
+    <main>
+      <div className="relative">
+        <ImgContainer src="/img-landing.png" alt="Description for image" />
+      </div>
+    </main>
+  );
 }
