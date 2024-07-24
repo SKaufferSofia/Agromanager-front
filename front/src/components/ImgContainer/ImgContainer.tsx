49d726ff8c7dfeb9cc67cf8780776e1b67067125
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-23 22:53:32 -0300

    "Rutas Navbar and Footer"

--- front/src/components/ImgContainer/ImgContainer.tsx
+++ front/src/components/ImgContainer/ImgContainer.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import Image from "next/image";
 
 interface ImgContainerProps {
-	src: string;
-	alt: string;
+  src: string;
+  alt: string;
 }
 
 const ImgContainer: React.FC<ImgContainerProps> = ({ src, alt }) => {
-	return (
-		<div className="relative w-full h-[calc(100vh-4rem)]">
-			<Image src={src} alt={alt} layout="fill" objectFit="cover" />
-		</div>
-	);
+  return (
+    <div className="relative w-full h-[calc(100vh)]">
+      <Image src={src} alt={alt} layout="fill" objectFit="cover" />
+    </div>
+  );
 };
 
 export default ImgContainer;
