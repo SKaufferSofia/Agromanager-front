8556a639b7af7303adbf7edf8ae8c843e6241788
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-24 15:56:27 -0300

    "Font and Colors general"

--- front/src/components/ImgContainer/ImgContainer.tsx
+++ front/src/components/ImgContainer/ImgContainer.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import Image from "next/image";
 
 interface ImgContainerProps {
-	src: string;
-	alt: string;
-	children: React.ReactNode;
+  src: string;
+  alt: string;
+  children?: React.ReactNode;
 }
 
 const ImgContainer: React.FC<ImgContainerProps> = ({ src, alt, children }) => {
-	return (
-		<div className="relative w-full h-[calc(70vh)]">
-			<Image src={src} alt={alt} layout="fill" objectFit="cover" />
-			<div className="absolute inset-0 flex items-center justify-center text-center p-4 bg-black bg-opacity-50">
-				{children}
-			</div>
-		</div>
-	);
+  return (
+    <div className="relative w-full h-[calc(70vh)]">
+      <Image src={src} alt={alt} layout="fill" objectFit="cover" />
+      <div className="absolute inset-0 flex items-center justify-center text-center p-4 bg-black bg-opacity-50">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default ImgContainer;
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        bgColor: "#E9E7DD",
+        textColor: "#585858",
+        navbarColor: "#636B3F",
+        footerColor: "#747C5D",
+      },
     },
   },
   plugins: [],
