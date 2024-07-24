36976666e4ef90c80b38364db993514097efc0e4
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-07-24 18:28:09 +0200

    fix button possition

--- front/src/components/MainButton/MainButton.tsx
+++ front/src/components/MainButton/MainButton.tsx
@@ -1,7 +1,7 @@
 const MainButton = () => {
 	return (
-		<div>
-			<button className="w-32 p-2 flex justify-center border-[#70823E] border-2 rounded-md shadow-sm text-sm font-medium text-[#70823E] bg-white hover:bg-gray-400 focus:ring-offset-2">
+		<div className="flex items-center justify-center">
+			<button className="w-32 p-2 flex justify-center border-[#70823E] border-2 rounded-md shadow-sm text-sm font-medium text-[#70823E] bg-white hover:bg-gray-100 focus:ring-offset-2">
 				Ver Más
 			</button>
 		</div>
