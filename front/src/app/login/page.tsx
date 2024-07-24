60fe6a4ce7fa306b6733e39a2fa30fe33383f219
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-07-24 09:04:09 +0200

    add login && register files

---
+++ front/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login: React.FC = () => {
+	return (
+		<div className="flex min-h-screen flex-col py-28">
+			<h1>Login</h1>
+		</div>
+	);
+};
+
+export default Login;
