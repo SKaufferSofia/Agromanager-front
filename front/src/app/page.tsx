99c394e58487402609b043e4463730c6c2544a4f
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-07-25 09:08:43 +0200

    fix button position

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -29,76 +29,81 @@ export default function Landing() {
 				<div className="w-full h-1/3 bg-bgColor">
 					<LandingCarousel />
 				</div>
-				<div className="flex w-full bg-white">
-					<div className="p-8 w-1/3 flex flex-col items-center text-center">
-						<Image
-							src={icon1}
-							alt={"icon"}
-							width={150}
-							height={150}
-						/>
-						<div className="text-textColor">
-							<h2 className="text-2xl mb-8 font-medium">
-								Lotes Info
-							</h2>
-							<p>
-								de relleno de las imprentas y archivos de texto.
-								Lorem Ipsum ha sido el texto de relleno estándar
-								de las industrias desde el año 1500, cuando un
-								impresor (N. del T. persona que se dedica a la
-								imprenta) desconocido usó una galería de textos
-								y los mezcló de tal manera que logró hacer un
-								libro de textos especimen.Landing Info
-							</p>
+				<div className=" w-full bg-white">
+					<div className="flex">
+						<div className="p-8 w-1/3 flex flex-col items-center text-center">
+							<Image
+								src={icon1}
+								alt={"icon"}
+								width={150}
+								height={150}
+							/>
+							<div className="text-textColor">
+								<h2 className="text-2xl mb-8 font-medium">
+									Lotes Info
+								</h2>
+								<p>
+									de relleno de las imprentas y archivos de
+									texto. Lorem Ipsum ha sido el texto de
+									relleno estándar de las industrias desde el
+									año 1500, cuando un impresor (N. del T.
+									persona que se dedica a la imprenta)
+									desconocido usó una galería de textos y los
+									mezcló de tal manera que logró hacer un
+									libro de textos especimen.Landing Info
+								</p>
+							</div>
+						</div>
+						<div className="p-8 w-1/3 flex flex-col items-center text-center">
+							<Image
+								src={icon2}
+								alt={"icon"}
+								width={150}
+								height={150}
+							/>
+							<div className="text-[#585858]">
+								<h2 className="text-2xl mb-8 font-medium">
+									Labores Info
+								</h2>
+								<p>
+									de relleno de las imprentas y archivos de
+									texto. Lorem Ipsum ha sido el texto de
+									relleno estándar de las industrias desde el
+									año 1500, cuando un impresor (N. del T.
+									persona que se dedica a la imprenta)
+									desconocido usó una galería de textos y los
+									mezcló de tal manera que logró hacer un
+									libro de textos especimen.Landing Info
+								</p>
+							</div>
+						</div>
+						<div className="p-8 w-1/3 flex flex-col items-center text-center">
+							<Image
+								src={icon3}
+								alt={"icon"}
+								width={150}
+								height={150}
+							/>
+							<div className="text-[#585858]">
+								<h2 className="text-2xl mb-8 font-medium">
+									Insumos Info
+								</h2>
+								<p>
+									de relleno de las imprentas y archivos de
+									texto. Lorem Ipsum ha sido el texto de
+									relleno estándar de las industrias desde el
+									año 1500, cuando un impresor (N. del T.
+									persona que se dedica a la imprenta)
+									desconocido usó una galería de textos y los
+									mezcló de tal manera que logró hacer un
+									libro de textos especimen.Landing Info
+								</p>
+							</div>
 						</div>
 					</div>
-					<div className="p-8 w-1/3 flex flex-col items-center text-center">
-						<Image
-							src={icon2}
-							alt={"icon"}
-							width={150}
-							height={150}
-						/>
-						<div className="text-[#585858]">
-							<h2 className="text-2xl mb-8 font-medium">
-								Labores Info
-							</h2>
-							<p>
-								de relleno de las imprentas y archivos de texto.
-								Lorem Ipsum ha sido el texto de relleno estándar
-								de las industrias desde el año 1500, cuando un
-								impresor (N. del T. persona que se dedica a la
-								imprenta) desconocido usó una galería de textos
-								y los mezcló de tal manera que logró hacer un
-								libro de textos especimen.Landing Info
-							</p>
-						</div>
+					<div className="flex justify-center p-8 ">
+						<MainButton text="Ver Más" path="/home" />
 					</div>
-					<div className="p-8 w-1/3 flex flex-col items-center text-center">
-						<Image
-							src={icon3}
-							alt={"icon"}
-							width={150}
-							height={150}
-						/>
-						<div className="text-[#585858]">
-							<h2 className="text-2xl mb-8 font-medium">
-								Insumos Info
-							</h2>
-							<p>
-								de relleno de las imprentas y archivos de texto.
-								Lorem Ipsum ha sido el texto de relleno estándar
-								de las industrias desde el año 1500, cuando un
-								impresor (N. del T. persona que se dedica a la
-								imprenta) desconocido usó una galería de textos
-								y los mezcló de tal manera que logró hacer un
-								libro de textos especimen.Landing Info
-							</p>
-						</div>
-					</div>
-				</div>
-				<div className="w-full flex justify-center p-8">
-					<MainButton text="Ver Más" path="/home" />
 				</div>
 			</div>
 		</main>
