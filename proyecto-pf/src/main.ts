edc5778ce4a915bddad39a768bf605fa039d7b75
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-25 15:55:39 -0300

    Back Port Change

--- proyecto-pf/src/main.ts
+++ proyecto-pf/src/main.ts
@@ -4,25 +4,28 @@ import { loggerMiddleware } from './middlewares/logger.middleware';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
+const PORT = process.env.DB_port;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(loggerMiddleware)
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }))
+  app.use(loggerMiddleware);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
-  .setTitle("AgroManager")
-  .setDescription("")
-  .setVersion("1.0.0")
-  .addBearerAuth()
-  .build()
+    .setTitle('AgroManager')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig)
-  SwaggerModule.setup("api", app, document)
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
