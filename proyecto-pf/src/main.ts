fbc2b834bc947def6396c142b6d4a8c48d2219ba
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-26 20:18:09 -0300

    Backend updated

--- proyecto-pf/src/main.ts
+++ proyecto-pf/src/main.ts
@@ -3,27 +3,27 @@ import { AppModule } from './app.module';
 import { loggerMiddleware } from './middlewares/logger.middleware';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  app.use(loggerMiddleware);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
+  app.use(loggerMiddleware)
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }))
+  app.use(cors())
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('AgroManager')
-    .setDescription('')
-    .setVersion('1.0.0')
-    .addBearerAuth()
-    .build();
+  .setTitle("AgroManager")
+  .setDescription("")
+  .setVersion("1.0.0")
+  .addBearerAuth()
+  .build()
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup("api", app, document)
 
-  await app.listen(4000);
+  await app.listen(process.env.APP_port);
 }
 bootstrap();
