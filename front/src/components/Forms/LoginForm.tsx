06e233031a4df0339a6120a533db450dea6f0206
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-26 16:12:06 -0300

    Register and Login petition completed

--- front/src/components/Forms/LoginForm.tsx
+++ front/src/components/Forms/LoginForm.tsx
@@ -1,54 +1,111 @@
-const LoginForm = () => {
-	return (
-		<div className="p-8 w-full flex flex-col min-h-screen justify-center items-center">
-			<div className="w-full max-w-md">
-				<h2 className="text-4xl text-textColor font-semibold mb-4 text-center">
-					Iniciar Sesión
-				</h2>
-				<h3 className="text-xl text-textColor font-semibold mb-4 text-center">
-					Comienza con tu prueba gratuita
-				</h3>
-				<form className="space-y-5 flex flex-col h-full">
-					<div>
-						<label className="block text-sm font-medium text-gray-700">
-							E-mail
-						</label>
-						<input
-							type="text"
-							name="email"
-							placeholder="E-mail"
-							className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
-						/>
-					</div>
-					<div>
-						<label className="block text-sm font-medium text-gray-700">
-							Contraseña
-						</label>
-						<input
-							type="password"
-							name="password"
-							placeholder="Contraseña"
-							className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
-						/>
-					</div>
+"use client";
+import { useRouter } from "next/navigation";
+import useForm from "@/hooks/useForm";
+import { PetitionLogin } from "@/lib/server/petitionUser";
+import { validateLogin } from "@/helpers/valitateLogin";
 
-					<div className="mt-auto flex justify-center">
-						<button className="w-32 p-2 flex justify-center border-footerColor border-2 rounded-md shadow-sm text-sm font-medium text-footerColor hover:bg-gray-200 focus:ring-offset-2">
-							SIGUIENTE
-						</button>
-					</div>
-				</form>
-				<div className="flex justify-center mt-4">
-					<a
-						href="/register"
-						className="text-textColor md:hover:scale-105 md:hover:ease-in-out"
-					>
-						No tenes una cuenta? Suscribite aquí
-					</a>
-				</div>
-			</div>
-		</div>
-	);
+const LoginForm = () => {
+  const router = useRouter();
+
+  const {
+    loginData,
+    setLoginData,
+    errorLogin,
+    setErrorLogin,
+    showPassword,
+    setShowPassword,
+  } = useForm();
+
+  const handleOnclickPassword = () => setShowPassword(!showPassword);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setLoginData({ ...loginData, [name]: value });
+    setErrorLogin(validateLogin({ ...loginData, [name]: value }));
+  };
+
+  const handleSumbit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (Object.keys(errorLogin).length === 0) {
+      const loginSuccess = await PetitionLogin(loginData);
+      if (loginSuccess) {
+        alert("Login exitoso");
+        router.push("/dashboard/plots");
+      }
+    } else {
+      alert("Complete todos los campos");
+    }
+  };
+
+  return (
+    <div className="p-8 w-full flex flex-col min-h-screen justify-center items-center">
+      <div className="w-full max-w-md mb-24">
+        <h2 className="text-4xl text-textColor font-semibold mb-4 text-center">
+          Iniciar Sesión
+        </h2>
+        <h3 className="text-xl text-textColor font-semibold mb-4 text-center">
+          Comienza con tu prueba gratuita
+        </h3>
+        <form
+          onSubmit={handleSumbit}
+          className="space-y-5 flex flex-col h-full"
+        >
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              E-mail
+              {!loginData.email && <span className="text-red-500"> * </span>}
+            </label>
+            <input
+              type="text"
+              name="email"
+              onChange={handleChange}
+              placeholder="E-mail"
+              className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Contraseña
+              {!loginData.password && <span className="text-red-500"> * </span>}
+            </label>
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              placeholder="Contraseña"
+              className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
+            />
+          </div>
+
+          <div className="mt-auto flex justify-center">
+            <button
+              type="submit"
+              className="w-32 p-2 flex justify-center border-footerColor border-2 rounded-md shadow-sm text-sm font-medium text-footerColor hover:bg-gray-200 focus:ring-offset-2"
+            >
+              INICIAR SESION
+            </button>
+          </div>
+          <div className="mt-1">
+            {!loginData.password && (
+              <p className=" text-sm text-red-500 font-medium ">
+                {" "}
+                (*) All fields are required
+              </p>
+            )}
+          </div>
+          <div className="flex justify-center mt-4 py-4">
+            <a
+              href="/register"
+              className="text-textColor md:hover:scale-105 md:hover:ease-in-out"
+            >
+              No tenes una cuenta? Suscribite aquí
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
