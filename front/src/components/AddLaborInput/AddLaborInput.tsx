cd3e51cc37d5cebe34592102352e4f92c03c6268
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-07-27 11:39:11 +0200

    add api request to AddLaborInput

--- front/src/components/AddLaborInput/AddLaborInput.tsx
+++ front/src/components/AddLaborInput/AddLaborInput.tsx
@@ -1,9 +1,44 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const AddLaborInput: React.FC = () => {
+	const [name, setName] = useState("");
+	const [contractor, setContractor] = useState("");
+	const [price, setPrice] = useState("");
+	const [surface, setSurface] = useState("");
+	const plotId = 20;
+
+	const handleSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
+		console.log("Nombre del Labor:", name);
+		console.log("Contratista:", contractor);
+		console.log("Precio:", price);
+		console.log("Superficie:", surface);
+		try {
+			const response = await axios.post(
+				"http://localhost:3001/plots/addLabor",
+				{
+					labor: {
+						name,
+						contractor,
+						price: parseFloat(price),
+						surface: parseFloat(surface),
+					},
+					plotId,
+				}
+			);
+
+			console.log(response.data);
+		} catch (error) {
+			console.error("Error creating labor:", error);
+		}
+	};
 	return (
 		<div>
-			<form className="flex ">
+			<form className="flex" onSubmit={handleSubmit}>
 				<div className="flex-1 mx-5">
 					<label className="block text-sm font-medium text-gray-700 ml-2">
 						Nombre del Labor
@@ -11,7 +46,9 @@ const AddLaborInput: React.FC = () => {
 					<input
 						type="text"
 						name="name"
-						placeholder="Nombre del insumo"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						placeholder="Nombre del labor"
 						className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
 					/>
 				</div>
@@ -22,6 +59,8 @@ const AddLaborInput: React.FC = () => {
 					<input
 						type="text"
 						name="contractor"
+						value={contractor}
+						onChange={(e) => setContractor(e.target.value)}
 						placeholder="Contratista"
 						className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
 					/>
@@ -33,6 +72,8 @@ const AddLaborInput: React.FC = () => {
 					<input
 						type="number"
 						name="price"
+						value={price}
+						onChange={(e) => setPrice(e.target.value)}
 						placeholder="Precio"
 						className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
 					/>
@@ -44,6 +85,8 @@ const AddLaborInput: React.FC = () => {
 					<input
 						type="number"
 						name="surface"
+						value={surface}
+						onChange={(e) => setSurface(e.target.value)}
 						placeholder="Cantidad"
 						className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
 					/>
