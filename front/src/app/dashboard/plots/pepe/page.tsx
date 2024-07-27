a7e5a6537ce49c31f1294aeb747f17cda176d0b8
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-07-27 23:25:08 +0200

    add logic to render plots in dashboard

--- front/src/app/dashboard/plots/pepe/page.tsx
+++ front/src/app/dashboard/plots/pepe/page.tsx
@@ -1,52 +1,46 @@
+"use client";
+
 import React from "react";
-import { ICreatePlot } from "@/interfaces/interfaces";
 import AddSupplyInput from "@/components/AddSupplyInput/AddSupplyInput";
 import AddLaborInput from "@/components/AddLaborInput/AddLaborInput";
 import DetailSupplyLaborCard from "@/components/DetailSupplyLaborCard/DetailSupplyLaborCard";
 import SideNavbar from "@/components/Navbar/sideNavbar";
-import plots from "@/helpers/plotsArray";
+import { useSelector } from "react-redux";
+import { IPlotsType } from "@/interfaces/interfaces";
 
-const mockPlots: ICreatePlot = {
-  id: 1,
-  surface: "50",
-  cereal: "Trigo",
-  user: "123e4567-e89b-12d3-a456-426614174000",
+const PlotDetailDashboard: React.FC = () => {
+	const plot = useSelector((state: any) => state.plot);
+	const plotId = plot.map((plot: IPlotsType) => plot.id);
+
+	return (
+		<div className=" w-screen h-full flex flex-col sm:flex-row">
+			<div className="w-1/6 mt-24 h-min-screen  bg-sideNavbarColor bg-opacity-20 ">
+				<SideNavbar plots={plotId} />
+			</div>
+			<div className="mt-36 w-5/6 ">
+				<h2 className="text-4xl font-semibold text-center mb-4">
+					Lote {plotId.cereal}
+				</h2>
+				<div className="m-10 flex justify-around text-lg font-semibold">
+					<p>Superficie: {plotId.surface} HA</p>
+					<p>Cereal: {plotId.cereal}</p>
+				</div>
+				<div className="p-8">
+					<h2 className="text-2xl font-semibold text-center mb-4">
+						Labores
+					</h2>
+					<AddLaborInput />
+					<h2 className="text-2xl font-semibold text-center mt-4 mb-4">
+						Insumos
+					</h2>
+					<AddSupplyInput />
+				</div>
+				<div className=" bg-altBgColor">
+					<DetailSupplyLaborCard />
+				</div>
+			</div>
+		</div>
+	);
 };
 
-const PlotDashboard: React.FC = () => {
-  // ! CORREGIR EL NOMBRE POR QUE SE PISA CON EL DASHBOARD QUE ESTA EN LA RUTA PLOTS
-
-  // const plot = useSelector((state: any) => state.plot);
-  // const plotId = plot.map((plot: IPlotsType) => plot.id);
-  //! para que traigas el id de los plots
-
-  return (
-    <div className=" w-screen h-full flex flex-col sm:flex-row">
-      <div className="w-1/6 mt-24 h-min-screen  bg-sideNavbarColor bg-opacity-20 ">
-        <SideNavbar plots={plots} />
-      </div>
-      <div className="mt-36 w-5/6 ">
-        <h2 className="text-4xl font-semibold text-center mb-4">
-          Lote {mockPlots.cereal}
-        </h2>
-        <div className="m-10 flex justify-around text-lg font-semibold">
-          <p>Superficie: {mockPlots.surface} HA</p>
-          <p>Cereal: {mockPlots.cereal}</p>
-        </div>
-        <div className="p-8">
-          <h2 className="text-2xl font-semibold text-center mb-4">Labores</h2>
-          <AddLaborInput />
-          <h2 className="text-2xl font-semibold text-center mt-4 mb-4">
-            Insumos
-          </h2>
-          <AddSupplyInput />
-        </div>
-        <div className=" bg-altBgColor">
-          <DetailSupplyLaborCard />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default PlotDashboard;
+export default PlotDetailDashboard;
