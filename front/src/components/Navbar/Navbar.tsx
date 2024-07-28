9c72764da50b480cfb98ab7e23899c3523169d77
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-28 01:42:40 -0300

    Navbar Responsive Updated

--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import Image from "next/image";
-import { usePathname } from "next/navigation";
 import React from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+  Card,
+} from "@material-tailwind/react";
+import { usePathname } from "next/navigation";
 import { useSelector } from "react-redux";
 import useUserData from "@/hooks/useUserData";
 import { useRouter } from "next/navigation";
 
-const Navbar: React.FC = () => {
+const NavbarComponent: React.FC = () => {
   const pathname = usePathname();
   const [bgColor, setBgColor] = React.useState("transparent");
   const isLogged = useSelector((state: any) => state.isLoggin);
@@ -39,78 +47,177 @@ const Navbar: React.FC = () => {
     }
   }, [pathname]);
 
+  const [openNav, setOpenNav] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <li>
+        <a
+          href="/home"
+          className="block py-2 px-3 text-white rounded md:bg-transparent md:hover:scale-105 md:hover:ease-in-out md:p-0"
+          aria-current="page"
+        >
+          INICIO
+        </a>
+      </li>
+      <li>
+        <a
+          href="/about"
+          className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:scale-105 md:hover:ease-in-out md:p-0"
+        >
+          SOBRE NOSOTROS
+        </a>
+      </li>
+      <li>
+        <a
+          href="/contact"
+          className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:scale-105 md:hover:ease-in-out md:p-0"
+        >
+          CONTACTO
+        </a>
+      </li>
+    </ul>
+  );
+
   return (
-    <div>
-      <nav
-        className={`fixed top-0 left-0 w-full ${bgColor} transition-colors ease-in-out duration-300`}
+    <div className="max-h-[768px] w-screen fixed ">
+      <Navbar
+        color="transparent"
+        className={`sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 ${bgColor} transition-colors ease-in-out duration-300 lg:px-16 lg:py-1`}
       >
-        {!isLogged ? (
-          <div className="flex flex-wrap items-center justify-between w-full">
-            <a href="/" className="flex items-center space-x-3">
-              <Image
-                src="/image4.png"
-                width={200}
-                height={200}
-                alt="agromanager Logo"
-              />
-            </a>
-            <div className="hidden w-full md:block md:w-auto md:ml-8">
-              <ul className="font-medium flex flex-col p-4 md:p-0  md:flex-row md:space-x-8 md:mt-0 md:border-0">
-                <li>
-                  <a
-                    href="/home"
-                    className="block py-2 px-3 text-white rounded md:bg-transparent md:hover:scale-105 md:hover:ease-in-out md:p-0"
-                    aria-current="page"
-                  >
-                    INICIO
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/about"
-                    className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:scale-105 md:hover:ease-in-out md:p-0"
-                  >
-                    SOBRE NOSOTROS
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/contact"
-                    className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:scale-105 md:hover:ease-in-out md:p-0"
-                  >
-                    CONTACTO
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="font-medium flex flex-col p-4 md:p-8 md:flex-row md:space-x-8">
+        <div className="flex items-center justify-between text-white">
+          <a href="/" className="flex items-center space-x-3">
+            <Image
+              src="/image4.png"
+              width={160}
+              height={160}
+              alt="agromanager Logo"
+            />
+          </a>
+          <div className="flex items-center gap-4">
+            <div className="mr-80">{navList}</div>
+            <div className="flex items-center gap-x-10">
               <a href="/login">
-                <p className="text-white md:hover:scale-105 md:hover:ease-in-out md:p-0">
+                <p className="text-white hidden  lg:inline-block  md:hover:scale-105 md:hover:ease-in-out md:p-0">
                   INICIAR SESIÓN
                 </p>
               </a>
               <a href="/register">
-                <p className="text-white md:hover:scale-105 md:hover:ease-in-out md:p-0">
+                <p className="text-white hidden  lg:inline-block md:hover:scale-105 md:hover:ease-in-out md:p-0">
                   REGISTRARSE
                 </p>
               </a>
             </div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
           </div>
-        ) : (
-          <div className="flex flex-wrap items-center justify-between w-full">
-            <Image
-              src="/image4.png"
-              width={200}
-              height={200}
-              alt="agromanager Logo"
-            />
-
-            <button onClick={handleLogOut}>CERRAR SESION</button>
+        </div>
+        <MobileNav open={openNav}>
+          {navList}
+          <div className="flex items-center gap-x-10">
+            <a href="/login">
+              <p className="text-white md:hover:scale-105 md:hover:ease-in-out md:p-0">
+                INICIAR SESIÓN
+              </p>
+            </a>
+            <a href="/register">
+              <p className="text-white md:hover:scale-105 md:hover:ease-in-out md:p-0">
+                REGISTRARSE
+              </p>
+            </a>
           </div>
-        )}
-      </nav>
+        </MobileNav>
+      </Navbar>
     </div>
   );
 };
 
-export default Navbar;
+export default NavbarComponent;
+// <div>
+//   <nav
+//     className={`fixed top-0 left-0 w-full ${bgColor} transition-colors ease-in-out duration-300`}
+//   >
+//     {!isLogged ? (
+//       <div className="flex flex-wrap items-center justify-between w-full">
+//         <a href="/" className="flex items-center space-x-3">
+//           <Image
+//             src="/image4.png"
+//             width={200}
+//             height={200}
+//             alt="agromanager Logo"
+//           />
+//         </a>
+//         <div className="hidden w-full md:block md:w-auto md:ml-8">
+//           <ul className="font-medium flex flex-col p-4 md:p-0  md:flex-row md:space-x-8 md:mt-0 md:border-0">
+//
+//           </ul>
+//         </div>
+//         <div className="font-medium flex flex-col p-4 md:p-8 md:flex-row md:space-x-8">
+// <a href="/login">
+//   <p className="text-white md:hover:scale-105 md:hover:ease-in-out md:p-0">
+//     INICIAR SESIÓN
+//   </p>
+// </a>
+// <a href="/register">
+//   <p className="text-white md:hover:scale-105 md:hover:ease-in-out md:p-0">
+//     REGISTRARSE
+//   </p>
+// </a>
+//         </div>
+//       </div>
+//     ) : (
+//       <div className="flex flex-wrap items-center justify-between w-full">
+//         <Image
+//           src="/image4.png"
+//           width={200}
+//           height={200}
+//           alt="agromanager Logo"
+//         />
+
+//         <button onClick={handleLogOut}>CERRAR SESION</button>
+//       </div>
+//     )}
+//   </nav>
+// </div>
