e8739240d3a4df16c172b82dc0b2f0ff10f5017d
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-29 15:37:24 -0300

    Backend Updated

--- proyecto-pf/src/configs/typeOrm.config.ts
+++ proyecto-pf/src/configs/typeOrm.config.ts
@@ -1,27 +1,27 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
-import { config as dotenvConfig } from 'dotenv';
-import { registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from "typeorm";
+import { config as dotenvConfig } from "dotenv";
+import { registerAs } from "@nestjs/config";
 import * as path from 'path';
 
-dotenvConfig({ path: '.env' });
-const algo = 12;
+dotenvConfig({ path: ".env"}); 
+const algo = 12
 
 const config = {
-  type: 'postgres',
-  database: process.env.DB_database,
-  // host: 'postgresdb',
-  host: process.env.DB_host,
-  port: parseInt(process.env.DB_port, 10),
-  username: process.env.DB_username,
-  password: process.env.DB_password,
-  synchronize: true,
-  logging: false,
-  //dropSchema: true,
-  autoLoadEntities: false,
-  entities: [path.join(__dirname, '../../dist/**/*.entity{.ts,.js}')],
-  migrations: [path.join(__dirname, '../../dist/migrations/*{.js,.ts}')],
+    type: 'postgres',
+    database: process.env.DB_database,
+    // host: 'postgresdb',
+    host:process.env.DB_host,
+    port: parseInt(process.env.DB_port, 10),
+    username: process.env.DB_username,
+    password: process.env.DB_password,
+    synchronize: true,
+    logging: false,
+    dropSchema: false,  
+    autoLoadEntities: false,
+    entities: [path.join(__dirname, '../../dist/**/*.entity{.ts,.js}')],
+    migrations: [path.join(__dirname, '../../dist/migrations/*{.js,.ts}')],
 };
 
-export default registerAs('typeorm', () => config);
+export default registerAs("typeorm", () => config);
 
 export const connectionSource = new DataSource(config as DataSourceOptions);
