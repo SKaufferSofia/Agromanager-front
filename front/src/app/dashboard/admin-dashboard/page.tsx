7a71aced1df91ffbc2c739c4c789e66e6b682b25
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-08-01 15:19:54 +0200

    change layot to a basic users display for an MVP

--- front/src/app/dashboard/admin-dashboard/page.tsx
+++ front/src/app/dashboard/admin-dashboard/page.tsx
@@ -5,29 +5,7 @@ const AdminDashboard: React.FC = () => {
 	return (
 		<div className="flex h-4/5">
 			<div className="flex-col flex-1 mt-24">
-				<div className="flex">
-					<div className="flex-1 m-4">
-						<AdminDashboardCard />
-					</div>
-					<div className="flex-1 m-4">
-						<AdminDashboardCard />
-					</div>
-				</div>
-				<div className="flex-col">
-					<div className="flex-1">
-						<div className="p-4 flex-1">
-							<AdminDashboardCard />
-						</div>
-					</div>
-					<div className="p-4 flex-1">
-						<div className="flex-1">
-							<AdminDashboardCard />
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="mt-24 flex h-screen flex-1">
-				<div className="p-4 flex-1 h-90">
+				<div className="flex-1 m-4">
 					<AdminDashboardCard />
 				</div>
 			</div>
