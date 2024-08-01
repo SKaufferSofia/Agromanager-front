90c643712d8becb75209f5be5608f706169e6f71
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-08-01 15:00:18 +0200

    add basic logic to render users name in card component

--- front/src/components/AdminDasboardCard/AdminDashboardCard.tsx
+++ front/src/components/AdminDasboardCard/AdminDashboardCard.tsx
@@ -1,29 +1,36 @@
 "use client";
 
 import { fetchAllUsers } from "@/lib/server/petitionAdminInfo";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const AdminDashboardCard = () => {
 	const token = useSelector((state: any) => state.token);
+	const [newArrayUsers, setNewArrayUsers] = useState<any[]>([]);
 
 	useEffect(() => {
 		const getAllUsers = async () => {
 			if (token) {
 				try {
 					const fetchedUsers = await fetchAllUsers(token);
-					console.log("############", fetchedUsers);
+					setNewArrayUsers(fetchedUsers);
 				} catch (error) {
 					console.error("Error fetching plots:", error);
 				}
 			}
 		};
-
 		getAllUsers();
 	}, [token]);
 	return (
 		<div>
-			<div className="bg-white shadow-md p-14 text-center"></div>
+			<div className="bg-white shadow-md p-14 text-center">
+				{newArrayUsers &&
+					newArrayUsers.map((user, index) => (
+						<div className="flex justify-around p-4" key={index}>
+							<div className="flex-1">{user.name}</div>
+						</div>
+					))}
+			</div>
 		</div>
 	);
 };
