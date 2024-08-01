133b6a93738bf801bab08630378a0458bfb7ba1c
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-08-01 13:29:18 +0200

    add basic layout for admin dashboard

--- front/src/app/dashboard/admin-dashboard/page.tsx
+++ front/src/app/dashboard/admin-dashboard/page.tsx
@@ -1,7 +1,37 @@
+import AdminDashboardCard from "@/components/AdminDasboardCard/AdminDashboardCard";
 import React from "react";
 
 const AdminDashboard: React.FC = () => {
-	return <div className="flex h-screen"></div>;
+	return (
+		<div className="flex h-4/5">
+			<div className="flex-col flex-1 mt-24">
+				<div className="flex">
+					<div className="flex-1 m-4">
+						<AdminDashboardCard />
+					</div>
+					<div className="flex-1 m-4">
+						<AdminDashboardCard />
+					</div>
+				</div>
+				<div className="flex-col">
+					<div className="flex-1">
+						<div className="p-4 flex-1">
+							<AdminDashboardCard />
+						</div>
+					</div>
+					<div className="p-4 flex-1">
+						<div className="flex-1">
+							<AdminDashboardCard />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="mt-24 flex h-screen flex-1">
+				<div className="p-4 flex-1 h-90">
+					<AdminDashboardCard />
+				</div>
+			</div>
+		</div>
+	);
 };
-
 export default AdminDashboard;
---
+++ front/src/components/AdminDasboardCard/AdminDashboardCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminDashboardCard = () => {
+	return (
+		<div>
+			<div className="bg-white shadow-md p-14 text-center"></div>
+		</div>
+	);
+};
+
+export default AdminDashboardCard;
