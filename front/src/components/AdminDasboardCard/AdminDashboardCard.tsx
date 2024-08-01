2826b28f2cda7a9264d627f5875dc1b2542495e4
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-08-01 16:55:35 +0200

    add button to edit users from admin dashboard

--- front/src/components/AdminDasboardCard/AdminDashboardCard.tsx
+++ front/src/components/AdminDasboardCard/AdminDashboardCard.tsx
@@ -22,6 +22,9 @@ const AdminDashboardCard = () => {
 		};
 		getAllUsers();
 	}, [token]);
+	const handleEditClick = () => {
+		alert("clicked");
+	};
 	return (
 		<div>
 			<div className="bg-white shadow-md">
@@ -44,6 +47,38 @@ const AdminDashboardCard = () => {
 							<div className="p-4 w-1/4">{user.placeName}</div>
 							<div className="p-4 w-1/4">
 								{user.active ? "Si" : "No"}
+							</div>
+							<div>
+								<button onClick={handleEditClick}>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										x="0px"
+										y="0px"
+										width="30"
+										height="30"
+										viewBox="0,0,300,150"
+									>
+										<g
+											fill="#719a2d"
+											fillRule="nonzero"
+											stroke="none"
+											strokeWidth="1"
+											strokeLinecap="butt"
+											strokeLinejoin="miter"
+											strokeMiterlimit="10"
+											strokeDasharray=""
+											strokeDashoffset="0"
+											fontFamily="none"
+											fontWeight="none"
+											fontSize="none"
+											textAnchor="none"
+										>
+											<g transform="scale(4,4)">
+												<path d="M39.086,17.914l7,7l-24.581,24.581l-9.201,4.412c-1.367,0.457 -2.668,-0.844 -2.211,-2.211l4.412,-9.201zM41.914,15.086l4.5,-4.5c0.781,-0.781 2.047,-0.781 2.828,0l4.172,4.172c0.781,0.781 0.781,2.047 0,2.828l-4.5,4.5z"></path>
+											</g>
+										</g>
+									</svg>
+								</button>
 							</div>
 						</div>
 					))}
