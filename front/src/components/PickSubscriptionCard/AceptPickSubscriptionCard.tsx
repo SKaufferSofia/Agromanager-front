6949dcc022de7baadd253f1abbdbaba37455e0f0
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-08-02 14:11:44 -0300

    payment updated

--- front/src/components/PickSubscriptionCard/AceptPickSubscriptionCard.tsx
+++ front/src/components/PickSubscriptionCard/AceptPickSubscriptionCard.tsx
@@ -3,20 +3,12 @@
 import React, { useEffect, useState } from "react";
 import SelectedSubscriptionCard from "../SelectedSubcriptionCard/SelectedSubscriptionCard";
 import MainButton from "../MainButton/MainButton";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
-import { PetitionLogin } from "@/lib/server/petitionUser";
-import { saveToken, saveUserData, signIn } from "@/redux/reducer";
-import useUserData from "@/hooks/useUserData";
-import Cookies from "js-cookie";
 
 const AceptPickSubscriptionCard: React.FC = () => {
   const subscription = useSelector((state: any) => state.selectedSubscription);
-  const email = useSelector((state: any) => state.registerData.email);
-  const password = useSelector((state: any) => state.registerData.password);
-  const { saveTokenStorage, saveUserDataStorage } = useUserData();
   const router = useRouter();
-  const dispatch = useDispatch();
 
   const [isClient, setIsClient] = useState(false);
 
@@ -36,32 +28,9 @@ const AceptPickSubscriptionCard: React.FC = () => {
     );
   }
 
-  const handleSumbit = async () => {
-    const loginData = {
-      email,
-      password,
-    };
-
-    const loginSuccess = await PetitionLogin(
-      loginData,
-      (token) => {
-        dispatch(saveToken(token));
-        saveTokenStorage(token);
-      },
-      (login) => {
-        dispatch(signIn(login));
-      },
-      (data) => {
-        dispatch(saveUserData(data));
-        saveUserDataStorage(data);
-      },
-      (data) => Cookies.set("token", data, { expires: 30 })
-    );
-
-    if (loginSuccess) {
-      alert("Login exitoso");
-      router.push("/dashboard/plots");
-    }
+  const handleSumbit = () => {
+    alert("Login exitoso");
+    router.push("/dashboard/plots");
   };
 
   return (
