f0dcdce975ede6c621eeac3d1a01fe633e266e47
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-08-02 12:30:33 -0300

    updated

--- front/src/app/subscriptions/accept-subscription/page.tsx
+++ front/src/app/subscriptions/accept-subscription/page.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import SelectedSubscriptionCard from "@/components/SelectedSubcriptionCard/SelectedSubscriptionCard";
-import MainButton from "@/components/MainButton/MainButton";
-import subscriptions from "@/helpers/subscriptions";
 import AceptPickSubscriptionCard from "@/components/PickSubscriptionCard/AceptPickSubscriptionCard";
 
 const AcceptSubscription: React.FC = () => {
