93efef1e9d60597e487e9d0bf4cfdb6a1b458e60
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-08-03 00:22:08 -0300

    payment year updated

--- front/src/components/PickSubscriptionCard/AceptPickSubscriptionCard.tsx
+++ front/src/components/PickSubscriptionCard/AceptPickSubscriptionCard.tsx
@@ -3,18 +3,46 @@
 import React, { useEffect, useState } from "react";
 import SelectedSubscriptionCard from "../SelectedSubcriptionCard/SelectedSubscriptionCard";
 import MainButton from "../MainButton/MainButton";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
+import { getUserById } from "@/lib/server/petitionUser";
+import { saveUserData } from "@/redux/reducer";
+import useUserData from "@/hooks/useUserData";
 
 const AceptPickSubscriptionCard: React.FC = () => {
   const subscription = useSelector((state: any) => state.selectedSubscription);
+  const token = useSelector((state: any) => state.token);
+  const userId = useSelector((state: any) => state.userData.id);
+
   const router = useRouter();
+  const { saveUserDataStorage } = useUserData();
+
+  const dispatch = useDispatch();
 
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
     setIsClient(true);
   }, []);
+
+  useEffect(() => {
+    if (userId && token) {
+      const getUser = async () => {
+        try {
+          const user = await getUserById(userId, token, (user) => {
+            dispatch(saveUserData(user));
+            saveUserDataStorage(user);
+          });
+          dispatch(saveUserData(user));
+          saveUserDataStorage(user);
+        } catch (error) {
+          console.error("Error fetching user:", error);
+        }
+      };
+
+      getUser();
+    }
+  }, [token, userId, dispatch]);
 
   if (!isClient) {
     return null;
@@ -28,7 +56,7 @@ const AceptPickSubscriptionCard: React.FC = () => {
     );
   }
 
-  const handleSumbit = () => {
+  const handleSumbit = async () => {
     alert("Login exitoso");
     router.push("/dashboard/plots");
   };
