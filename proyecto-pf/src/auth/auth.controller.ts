379e3b0677fb8cffe046fc80dd8fe5a67d66a9e6
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-08-04 19:39:04 -0300

    Backend updated

--- proyecto-pf/src/auth/auth.controller.ts
+++ proyecto-pf/src/auth/auth.controller.ts
@@ -1,37 +1,49 @@
-import { Body, Controller, HttpCode, Param, ParseUUIDPipe, Post, Put } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { AuthService } from "./auth.service";
-import { CreateUserDto } from "src/users/dto/create-user.dto";
-import { UUID } from "crypto";
-import { giveAdminDecorator, signInDecorator, signUpDecorator } from "./auth.decorators";
-import { SignInDto } from "./dto/signIn.dto";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UUID } from 'crypto';
+import {
+  giveAdminDecorator,
+  signInDecorator,
+  signUpDecorator,
+} from './auth.decorators';
+import { SignInDto } from './dto/signIn.dto';
 
-
-
-
-@ApiTags("Auth")
-@Controller("auth")
+@ApiTags('Auth')
+@Controller('auth')
 export class AuthController {
-    constructor(
-        private readonly AuthService: AuthService
-    ) {}
-    
+  constructor(private readonly AuthService: AuthService) {}
 
-    @Post("signup")
-    @signUpDecorator()
-    async signUp(@Body() createUserDto: CreateUserDto) {
-        return await this.AuthService.signUp(createUserDto)
-    }
+  @Post('signup')
+  @signUpDecorator()
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return await this.AuthService.signUp(createUserDto);
+  }
 
-    @Post("signin")
-    @signInDecorator()
-    signIn(@Body() signInDto : SignInDto) {
-        return this.AuthService.signIn(signInDto)
-    }
+  @Post('signin')
+  @signInDecorator()
+  signIn(@Body() signInDto: SignInDto) {
+    return this.AuthService.signIn(signInDto);
+  }
 
-    @Put("id")
-    @giveAdminDecorator()
-    giveAdmin(@Param("id", ParseUUIDPipe) id: UUID){
-        return this.AuthService.giveAdmin(id)
-    }
+  @Put('id')
+  @giveAdminDecorator()
+  giveAdmin(@Param('id', ParseUUIDPipe) id: UUID) {
+    return this.AuthService.giveAdmin(id);
+  }
+
+  @Post('google')
+  googleAuth(@Body() googleUser) {
+    console.log(googleUser);
+    return this.AuthService.googleAuth(googleUser);
+  }
 }
--- proyecto-pf/src/auth/auth.service.ts
+++ proyecto-pf/src/auth/auth.service.ts
@@ -29,9 +29,11 @@ export class AuthService {
 
     async signIn(signInDto: SignInDto) {
         const userFound = await this.UsersRepository.getUserByEmail(signInDto.email)
-
         if(!userFound){
             throw new BadRequestException("Credenciales incorrectas")
+        }
+        if(!userFound.password){
+            throw new BadRequestException("Inicia Sesion con google")
         }
         const confirmPassword: boolean = await bcrypt.compare(signInDto.password, userFound.password) 
         if (!confirmPassword){
@@ -53,5 +55,35 @@ export class AuthService {
 
     giveAdmin(id: UUID){
         return this.UsersRepository.giveAdmin(id)
+    }
+
+
+
+    async googleAuth(googleUser) { 
+        const user = await this.UsersRepository.getUserByEmail(googleUser.email)
+        if (!user) {
+            const user = (await this.UsersRepository.createUserGoogle(googleUser)).rest
+            const payload = {
+                sub: user.id,
+                email: user.email,
+                roles: user.roles
+            }
+
+            const token = this.JwtService.sign(payload)
+
+        return { message: 'registro y Sesion iniciada correctamente', token, isLoggin: true, user };
+        } else if (user) {
+            if (user.googleId !== googleUser.id) {throw new BadRequestException("El id del usuario es incorrecto")}
+
+            const payload = {
+                sub: user.id,
+                email: user.email,
+                roles: user.roles
+            }
+
+            const token = this.JwtService.sign(payload)
+
+        return { message: 'Sesion iniciada correctamente', token, isLoggin: true, user };
+        }  
     }
 }
