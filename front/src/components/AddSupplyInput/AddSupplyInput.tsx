a2b7e51dfbb9408b6dedd9e352476b27bc47e8c8
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-08-06 11:27:29 +0200

    change minimun value in inputs

--- front/src/components/AddSupplyInput/AddSupplyInput.tsx
+++ front/src/components/AddSupplyInput/AddSupplyInput.tsx
@@ -4,10 +4,10 @@ import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { NEXT_PUBLIC_API_URL } from "@/lib/server/envs";
 import {
-  Category,
-  IPlotsType,
-  Supply,
-  SupplyApplied,
+	Category,
+	IPlotsType,
+	Supply,
+	SupplyApplied,
 } from "@/interfaces/interfaces";
 import { RootState } from "@/redux/store";
 import { saveStock, updateSupplies } from "@/redux/reducer";
@@ -17,180 +17,190 @@ import { fetchSupplies } from "@/lib/server/petitionStock";
 import useDataStock from "@/hooks/useDataStock";
 
 interface AddSupplyInputProps {
-  plotId: string;
+	plotId: string;
 }
 
 const AddSupplyInput: React.FC<AddSupplyInputProps> = ({ plotId }) => {
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [supplies, setSupplies] = useState<Supply[]>([]);
-  const [filteredSupplies, setFilteredSupplies] = useState<Supply[]>([]);
-  const [activeCategory, setActiveCategory] = useState<Category | null>(null);
-  const [supplyId, setSupplyId] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const userId = useSelector((state: RootState) => state.userData.id);
-  const token = useSelector((state: RootState) => state.token);
-  const dispatch = useDispatch();
-  const { saveStockStorage } = useDataStock();
-  const { updatePlotsStorageWithSupplies, addSuppliesAppliedStorage } =
-    useDataPlot();
+	const [categories, setCategories] = useState<Category[]>([]);
+	const [supplies, setSupplies] = useState<Supply[]>([]);
+	const [filteredSupplies, setFilteredSupplies] = useState<Supply[]>([]);
+	const [activeCategory, setActiveCategory] = useState<Category | null>(null);
+	const [supplyId, setSupplyId] = useState("");
+	const [quantity, setQuantity] = useState("");
+	const userId = useSelector((state: RootState) => state.userData.id);
+	const token = useSelector((state: RootState) => state.token);
+	const dispatch = useDispatch();
+	const { saveStockStorage } = useDataStock();
+	const { updatePlotsStorageWithSupplies, addSuppliesAppliedStorage } =
+		useDataPlot();
 
-  useEffect(() => {
-    const getSuppliesByUser = async () => {
-      if (userId && token) {
-        try {
-          const response = await fetchSupplies(userId, token, (supplies) => {
-            dispatch(saveStock(supplies));
-            saveStockStorage(supplies);
-          });
-          setSupplies(response);
+	useEffect(() => {
+		const getSuppliesByUser = async () => {
+			if (userId && token) {
+				try {
+					const response = await fetchSupplies(
+						userId,
+						token,
+						(supplies) => {
+							dispatch(saveStock(supplies));
+							saveStockStorage(supplies);
+						}
+					);
+					setSupplies(response);
 
-          const uniqueCategories = Array.from(
-            new Set(response.map((supply: Supply) => supply.category.id))
-          ).map(
-            (id) =>
-              response.find((supply: Supply) => supply.category.id === id)
-                ?.category
-          ) as Category[];
-          setCategories(uniqueCategories);
-        } catch (error) {
-          console.error("Error fetching supplies:", error);
-        }
-      }
-    };
-    getSuppliesByUser();
-  }, [userId, token]);
+					const uniqueCategories = Array.from(
+						new Set(
+							response.map((supply: Supply) => supply.category.id)
+						)
+					).map(
+						(id) =>
+							response.find(
+								(supply: Supply) => supply.category.id === id
+							)?.category
+					) as Category[];
+					setCategories(uniqueCategories);
+				} catch (error) {
+					console.error("Error fetching supplies:", error);
+				}
+			}
+		};
+		getSuppliesByUser();
+	}, [userId, token]);
 
-  const handleCategoryChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const selectedCategoryId = event.target.value;
-    const selectedCategory =
-      categories.find((category) => category.id === selectedCategoryId) || null;
-    setActiveCategory(selectedCategory);
-    if (selectedCategory) {
-      const suppliesByCategory = supplies.filter(
-        (supply) => supply.category.id === selectedCategory.id
-      );
-      setFilteredSupplies(suppliesByCategory);
-    } else {
-      setFilteredSupplies(supplies);
-    }
-  };
+	const handleCategoryChange = (
+		event: React.ChangeEvent<HTMLSelectElement>
+	) => {
+		const selectedCategoryId = event.target.value;
+		const selectedCategory =
+			categories.find((category) => category.id === selectedCategoryId) ||
+			null;
+		setActiveCategory(selectedCategory);
+		if (selectedCategory) {
+			const suppliesByCategory = supplies.filter(
+				(supply) => supply.category.id === selectedCategory.id
+			);
+			setFilteredSupplies(suppliesByCategory);
+		} else {
+			setFilteredSupplies(supplies);
+		}
+	};
 
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
+	const handleSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
 
-    try {
-      const response = await axios.post(
-        `${NEXT_PUBLIC_API_URL}/plots/addSupply`,
-        {
-          supplyId,
-          plotId,
-          quantity,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      let updatedSuppliesArray: SupplyApplied[] = [];
+		try {
+			const response = await axios.post(
+				`${NEXT_PUBLIC_API_URL}/plots/addSupply`,
+				{
+					supplyId,
+					plotId,
+					quantity,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
+			let updatedSuppliesArray: SupplyApplied[] = [];
 
-      updatedSuppliesArray = [...updatedSuppliesArray, response.data];
-      dispatch(updateSupplies(updatedSuppliesArray));
-      addSuppliesAppliedStorage(response.data);
-      updatePlotsStorageWithSupplies(plotId, updatedSuppliesArray);
+			updatedSuppliesArray = [...updatedSuppliesArray, response.data];
+			dispatch(updateSupplies(updatedSuppliesArray));
+			addSuppliesAppliedStorage(response.data);
+			updatePlotsStorageWithSupplies(plotId, updatedSuppliesArray);
 
-      // console.log("Updated supplies:", updatedSupplies);
+			// console.log("Updated supplies:", updatedSupplies);
 
-      // dispatch(updateSupplies(response.data));
-      // updatePlotsStorageWithSupplies(plotId, response.data);
-      // console.log("Updated supplies:", updatedSupplies);
+			// dispatch(updateSupplies(response.data));
+			// updatePlotsStorageWithSupplies(plotId, response.data);
+			// console.log("Updated supplies:", updatedSupplies);
 
-      // dispatch(updateSupplies({ plotId, supplies: updatedSupplies }));
-      toast.success("Insumo Agregado", {
-        className: "mt-20 text-white bg-footerColor font-semibold text-xl",
-        duration: 2000,
-      });
-      setActiveCategory(null);
-      setQuantity("");
-      setFilteredSupplies([]);
-    } catch (error) {
-      if (axios.isAxiosError(error) && error.response) {
-        const axiosError = error.response.data.message;
-        toast.warning("Debes completar todos los campos", {
-          className: "bg-red-500 text-white text-lg ",
-          duration: 3000,
-        });
-      }
-    }
-  };
+			// dispatch(updateSupplies({ plotId, supplies: updatedSupplies }));
+			toast.success("Insumo Agregado", {
+				className:
+					"mt-20 text-white bg-footerColor font-semibold text-xl",
+				duration: 2000,
+			});
+			setActiveCategory(null);
+			setQuantity("");
+			setFilteredSupplies([]);
+		} catch (error) {
+			if (axios.isAxiosError(error) && error.response) {
+				const axiosError = error.response.data.message;
+				toast.warning("Debes completar todos los campos", {
+					className: "bg-red-500 text-white text-lg ",
+					duration: 3000,
+				});
+			}
+		}
+	};
 
-  return (
-    <div>
-      <form className="flex" onSubmit={handleSubmit}>
-        <div className="flex-1 mx-5">
-          <label className="block text-sm font-medium text-gray-700 ml-2">
-            Categoría
-          </label>
-          <select
-            name="category"
-            className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
-            onChange={handleCategoryChange}
-            value={activeCategory ? activeCategory.id : ""}
-          >
-            <option value="">Selecciona una categoría</option>
-            {categories &&
-              categories.map((category) => (
-                <option key={category.id} value={category.id}>
-                  {category.name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div className="flex-1 mx-2">
-          <label className="block text-sm font-medium text-gray-700 ml-2">
-            Nombre del insumo
-          </label>
-          <select
-            name="supply"
-            className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
-            onChange={(e) => setSupplyId(e.target.value)}
-            value={supplyId}
-          >
-            <option value="">Selecciona un insumo</option>
-            {filteredSupplies &&
-              filteredSupplies.map((supply) => (
-                <option key={supply.id} value={supply.id}>
-                  {supply.name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div className="flex-1 mx-2">
-          <label className="block text-sm font-medium text-gray-700 ml-2">
-            Cantidad
-          </label>
-          <input
-            type="number"
-            name="quantity"
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-            placeholder="Cantidad"
-            className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
-          />
-        </div>
-        <div className="mt-5">
-          <button
-            type="submit"
-            className="w-25 h-9 p-2 flex items-center justify-center  border-footerColor border-2 rounded-md shadow-sm text-md font-medium text-footerColor hover:bg-footerColor hover:text-white hover:ease-in-out focus:ring-offset-2"
-          >
-            Agregar
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<form className="flex" onSubmit={handleSubmit}>
+				<div className="flex-1 mx-5">
+					<label className="block text-sm font-medium text-gray-700 ml-2">
+						Categoría
+					</label>
+					<select
+						name="category"
+						className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
+						onChange={handleCategoryChange}
+						value={activeCategory ? activeCategory.id : ""}
+					>
+						<option value="">Selecciona una categoría</option>
+						{categories &&
+							categories.map((category) => (
+								<option key={category.id} value={category.id}>
+									{category.name}
+								</option>
+							))}
+					</select>
+				</div>
+				<div className="flex-1 mx-2">
+					<label className="block text-sm font-medium text-gray-700 ml-2">
+						Nombre del insumo
+					</label>
+					<select
+						name="supply"
+						className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
+						onChange={(e) => setSupplyId(e.target.value)}
+						value={supplyId}
+					>
+						<option value="">Selecciona un insumo</option>
+						{filteredSupplies &&
+							filteredSupplies.map((supply) => (
+								<option key={supply.id} value={supply.id}>
+									{supply.name}
+								</option>
+							))}
+					</select>
+				</div>
+				<div className="flex-1 mx-2">
+					<label className="block text-sm font-medium text-gray-700 ml-2">
+						Cantidad
+					</label>
+					<input
+						type="number"
+						name="quantity"
+						value={quantity}
+						min="0"
+						onChange={(e) => setQuantity(e.target.value)}
+						placeholder="Cantidad"
+						className="p-2 w-full flex justify-center border border-gray-300 rounded-sm shadow-sm sm:text-sm"
+					/>
+				</div>
+				<div className="mt-5">
+					<button
+						type="submit"
+						className="w-25 h-9 p-2 flex items-center justify-center  border-footerColor border-2 rounded-md shadow-sm text-md font-medium text-footerColor hover:bg-footerColor hover:text-white hover:ease-in-out focus:ring-offset-2"
+					>
+						Agregar
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default AddSupplyInput;
--- front/src/components/PlotPanel/PlotPanel.tsx
+++ front/src/components/PlotPanel/PlotPanel.tsx
@@ -11,109 +11,120 @@ import Map from "../WeatherComponent/WeatherMapComponent";
 import MainButton from "../MainButton/MainButton";
 
 const PlotPanel: React.FC<PlotPanelProps> = ({ plots, setPlots }) => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const { addPlotsStorage } = useDataPlot();
+	const { addPlotsStorage } = useDataPlot();
 
-  const [cereal, setCereal] = useState("");
-  const [surface, setSurface] = useState("");
+	const [cereal, setCereal] = useState("");
+	const [surface, setSurface] = useState("");
 
-  const userId = useSelector((state: any) => state.userData.id);
-  const token = useSelector((state: any) => state.token);
-  const latitude = useSelector((state: any) => state.latitude);
-  const longitude = useSelector((state: any) => state.longitude);
+	const userId = useSelector((state: any) => state.userData.id);
+	const token = useSelector((state: any) => state.token);
+	const latitude = useSelector((state: any) => state.latitude);
+	const longitude = useSelector((state: any) => state.longitude);
 
-  const handleCerealChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCereal(event.target.value);
-  };
+	const handleCerealChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setCereal(event.target.value);
+	};
 
-  const handleSurfaceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSurface(event.target.value);
-  };
+	const handleSurfaceChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setSurface(event.target.value);
+	};
 
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    if (!cereal || !surface) {
-      toast.warning("Debes completar todos lo campos", {
-        className: "bg-red-500 text-white text-lg ",
-        duration: 3000,
-      });
-      return;
-    }
-    const newPlot = { cereal, surface, latitude, longitude };
-    try {
-      const createdPlot = await createPlot(newPlot, userId, token, (data) => {
-        dispatch(addPlot(data));
-        addPlotsStorage(data);
-      });
-      if (createdPlot) {
-        setPlots((prevPlots) => [...prevPlots, createdPlot]);
-        setCereal("");
-        setSurface("");
-      }
-    } catch (error) {
-      toast.warning("Error al crear el lote", {
-        className: "bg-red-500 text-white text-lg ",
-        duration: 3000,
-      });
-    }
-  };
+	const handleSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
+		if (!cereal || !surface) {
+			toast.warning("Debes completar todos lo campos", {
+				className: "bg-red-500 text-white text-lg ",
+				duration: 3000,
+			});
+			return;
+		}
+		const newPlot = { cereal, surface, latitude, longitude };
+		try {
+			const createdPlot = await createPlot(
+				newPlot,
+				userId,
+				token,
+				(data) => {
+					dispatch(addPlot(data));
+					addPlotsStorage(data);
+				}
+			);
+			if (createdPlot) {
+				setPlots((prevPlots) => [...prevPlots, createdPlot]);
+				setCereal("");
+				setSurface("");
+			}
+		} catch (error) {
+			toast.warning("Error al crear el lote", {
+				className: "bg-red-500 text-white text-lg ",
+				duration: 3000,
+			});
+		}
+	};
 
-  return (
-    <div className="w-full max-w-screen-lg min-h-screen mx-auto p-4 flex flex-col">
-      <div className="mb-8">
-        <h1 className="text-2xl text-gray-900 font-bold text-start uppercase">
-          Todos tus lotes
-          <span className="poppins-extralight"> En un solo lugar.</span>
-        </h1>
-        <h2 className="text-lg text-gray-800 poppins-regular mt-10">
-          Agregar un nuevo lote
-        </h2>
-        <form
-          className="flex flex-col md:flex-row gap-4 mt-4"
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-col sm:flex-row sm:gap-4">
-            <input
-              type="text"
-              className="px-4 py-2 border rounded-lg text-gray-700"
-              placeholder="Tipo de cereal"
-              value={cereal}
-              onChange={handleCerealChange}
-            />
-            <input
-              type="text"
-              className="px-4 py-2 border rounded-lg text-gray-700"
-              placeholder="Superficie en Ha"
-              value={surface}
-              onChange={handleSurfaceChange}
-            />
-          </div>
-          <MainButton text="Crear Lote" />
-          {/* <button
+	return (
+		<div className="w-full max-w-screen-lg min-h-screen mx-auto p-4 flex flex-col">
+			<div className="mb-8">
+				<h1 className="text-2xl text-gray-900 font-bold text-start uppercase">
+					Todos tus lotes
+					<span className="poppins-extralight">
+						{" "}
+						En un solo lugar.
+					</span>
+				</h1>
+				<h2 className="text-lg text-gray-800 poppins-regular mt-10">
+					Agregar un nuevo lote
+				</h2>
+				<form
+					className="flex flex-col md:flex-row gap-4 mt-4"
+					onSubmit={handleSubmit}
+				>
+					<div className="flex flex-col sm:flex-row sm:gap-4">
+						<input
+							type="text"
+							className="px-4 py-2 border rounded-lg text-gray-700"
+							placeholder="Tipo de cereal"
+							value={cereal}
+							onChange={handleCerealChange}
+						/>
+						<input
+							type="text"
+							className="px-4 py-2 border rounded-lg text-gray-700"
+							placeholder="Superficie en Ha"
+							min="0"
+							value={surface}
+							onChange={handleSurfaceChange}
+						/>
+					</div>
+					<MainButton text="Crear Lote" />
+					{/* <button
             type="submit"
             className="px-4 py-2 bg-blue-600 text-white rounded-lg"
           >
             Crear lote
           </button> */}
-        </form>
-        <Map />
-      </div>
+				</form>
+				<Map />
+			</div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
-        {plots.map((plot: IPlotsType) => (
-          <PlotDetailCard
-            key={plot.id}
-            id={plot.id}
-            surface={plot.surface}
-            cereal={plot.cereal}
-            labors={plot.labors}
-            supplies={plot.supplies}
-          />
-        ))}
-      </div>
-    </div>
-  );
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
+				{plots.map((plot: IPlotsType) => (
+					<PlotDetailCard
+						key={plot.id}
+						id={plot.id}
+						surface={plot.surface}
+						cereal={plot.cereal}
+						labors={plot.labors}
+						supplies={plot.supplies}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default PlotPanel;
