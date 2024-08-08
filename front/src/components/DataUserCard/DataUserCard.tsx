7737b108ebf52e9faa23bf892a219966d6a58c80
ByteBiteChef <144017722+ByteBiteChef@users.noreply.github.com>
2024-08-08 11:49:41 +0200

    adjust basic style in data cards

--- front/src/components/DataUserCard/DataUserCard.tsx
+++ front/src/components/DataUserCard/DataUserCard.tsx
@@ -6,10 +6,12 @@ interface ICardData {
 }
 const DataUserCard: React.FC<ICardData> = ({ dataContent, title }) => {
 	return (
-		<div className="border rounded-md shadow-md p-4 bg-white justify-around h-24 mb-4">
-			<div className="flex flex-col">
-				<div className="font-bold">{title}</div>
-				<div>{dataContent}</div>
+		<div className="border rounded-md shadow-md p-4 bg-white justify-around h-24 mb-8">
+			<div className="flex flex-col p-2 ">
+				<div className="font-bold text-2xl text-center">
+					{dataContent}
+				</div>
+				<div className="font-ligth text-center">{title}</div>
 			</div>
 		</div>
 	);
